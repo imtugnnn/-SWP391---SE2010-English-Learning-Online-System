1e2c0727ba203c38702442c25f991d741cc655c0
imtugnnn <lamtung0307@gmail.com>
2026-06-05 01:59:17 +0700

    Update Program.cs

--- EnglishLearningOnlineSystem/EnglishLearningOnlineSystem/Program.cs
+++ EnglishLearningOnlineSystem/EnglishLearningOnlineSystem/Program.cs
@@ -58,12 +58,12 @@ builder.Services
         options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
         options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
     })
-    .AddCookie();
-    // .AddGoogle(options =>
-    // {
-    //     options.ClientId = builder.Configuration["Authentication:Google:ClientId"]!;
-    //     options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]!;
-    // });
+    .AddCookie()
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Authentication:Google:ClientId"]!;
+        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]!;
+    });
 // ===================================================================================
 
 var app = builder.Build();
